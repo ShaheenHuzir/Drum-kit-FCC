63761ca9e7489a7883f6aff9100b63d99d3141ad
ShaheenHuzir <40265125+ShaheenHuzir@users.noreply.github.com>
2020-08-01 21:58:03 +0300

    fcc-test-added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Drumkit from './components/drumkit/drumkit';
+import ReactFCCtest from 'react-fcctest';
 
 class App extends React.Component {
   render() {
     return (
       <div className='App'>
         <Drumkit />
+        <ReactFCCtest />
       </div>
     );
   }
